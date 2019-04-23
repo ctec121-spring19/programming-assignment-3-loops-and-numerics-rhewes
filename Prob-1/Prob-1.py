e657d6b4a4c45a255c3f2a10676eef24cdafe235
Riley Hewes <hewesrj@gmail.com>
2019-04-22 23:44:04 -0700

    Solved problems 1 and 3.

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,7 +2,7 @@
 #   Programming Assignment 3
 #     Prob-1.py
 
-# YOUR NAME
+# Riley Hewes
 
 def example():
     print("\nExample Output")
@@ -19,16 +19,21 @@ def example():
 
 def studentCode():
     # replace <name> with your name
-    print("\n<name>'s Output")
+    print("\nRiley's Output")
     # output the numbers between from 6 to 10 using the range() function
-    
+    for x in range(6,11):
+        print(x)
     # print a blank line
     print()
 
     # print the even numbers between 1 and 10 using a sequence
-
+    for y in (2,4,6,8,10):
+        print(y)
+        
+    print()
     # use the range function to print out the odd numbers between 1 and 10
-
+    for z in range(1,11,2):
+        print(z)
 example()
 studentCode()
 
--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,29 +2,35 @@
 #   Programming Assignment 3
 #     Prob-3.py
 
-# YOUR NAME
+# Riley Hewes
 
 # import the math library
-
+import math
 def studentCode():
     print("\nStudent Ouutput")
     
     # print the value of pi
+    print(math.pi)
 
     x = 12.345
     y = 9.876
 
     # print the value of the square root of x + y
+    print(math.sqrt(x+y))
 
     x = 3.1
     # print the ceil() of x
+    print(math.ceil(x))
 
     # print the floor() of x
+    print(math.floor(x))
 
     x = 3.9
     # print the ceil() of x
+    print(math.ceil(x))
 
     # print the floor() of x
+    print(math.floor(x))
 
     print()
 
